beb2242ddc01965f027328f45ecf423fd5afda8e
Rubaiyat Yameen <ryameen@gmail.com>
2020-04-29 14:29:41 +0600

    Wrote notes describing the project.

---
+++ Scripts/Project Description.R
@@ -0,0 +1,30 @@
+# This Project was made as a test to link a project that was
+#  - made locally in my computer in RStudio
+#  - Was not under verison control
+#      - meaning that it was not linked to Git
+#      - and not linked to GitHub
+# So the following steps were taken:
+# 1. A new project was created in RStudio
+# 2. This project was linked to Git. We did this by:
+#     i. Open Git Bash
+#     ii. Change directory to the directory where this new project is in my harddrive
+#         (cd F:/name/of/my/directory)
+#     iii. Initialize this directory as git repository (git init)
+#     iv. Add all the files in this directory to my local repository (git add .)
+#     v. Commit these changes to the git repository (git commit -m "Initial commit")
+# 3. This project was then linked to GitHub. We did this:
+#     i. Go to GitHub.com, and again, create a new repository:
+#         a) Make sure the name is the exact same as your R project;
+#         b) Do NOT initialize a README file, .gitignore, or license.
+#         c) Click Create Repository
+#         d) Upon creating the repository, a page will appear which will have an option
+#           to "push an existing repository from the command line
+#         e) In Git Bash or Terminal, copy and paste these lines of code to 
+#           link your repository with GitHub
+#         f) Refresh the GitHub page.
+#         g) When we re-open our project in RStudio, we can now have access to 
+#             the Git tab in the upper right quadrant and can push to GitHub from 
+#             within RStudio any future changes.
+#
+#
+#
